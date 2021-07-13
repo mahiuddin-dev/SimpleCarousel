e5aa069526fbad6c2ee39ed80c145a9b8c917b25
codermahiuddin <codermahiuddin@gmail.com>
2021-07-13 10:09:41 +0600

    updated

--- Carousel/settings.py
+++ Carousel/settings.py
@@ -12,8 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = 'django-insecure-sflu6jp_^c$%6u80ok3y^^gae2n)2akjng3l!z=r*e218ht-5j'
+SECRET_KEY = config('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
